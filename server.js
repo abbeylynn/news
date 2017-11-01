f6298f0e64adaccd0ec67ec5073c856ccf109ef5
Abbey Hudson <a_hudson2@yahoo.com>
2017-11-01 10:16:10 -0700

    changes for heroku

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news"
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI) {
+mongoose.connect("mongodb://localhost/news", {
   useMongoClient: true
-};
+});
 
 // Routes
 
